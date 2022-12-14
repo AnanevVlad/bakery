907420852b9a3dadc8727c011823791e79634916
Vlad <ananevvlad12@gmail.com>
2022-12-14 22:34:18 +0200

    newScript

--- js/scroll.js
+++ js/scroll.js
@@ -1,19 +1,27 @@
-let lastScroll = 0;
-const defaultOffset = 200;
-const header = document.querySelector('.ctn');
 
-const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
-const containHide = () => header.classList.contains('hide');
 
-window.addEventListener('scroll', () => {
-    if(scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
-        //scroll down
-        header.classList.add('hide');
-    }
-    else if(scrollPosition() < lastScroll && containHide()){
-        //scroll up
-        header.classList.remove('hide');
-    }
 
-    lastScroll = scrollPosition();
-})
+
+// Создаем новый observer (наблюдатель)
+let observer = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+// Выводим в консоль сам элемент
+        console.log(entry.target);
+// Выводим в консоль true (если элемент виден) или false (если нет)
+        console.log(entry.isIntersecting);
+        if (!entry.isIntersecting)
+        {
+            ctn.classList.add('hide');
+        }
+        if (entry.isIntersecting)
+        {
+            ctn.classList.remove('hide');
+        }
+    });
+});
+
+// Задаем элемент для наблюдения
+let el = document.querySelector('header');
+const ctn = document.querySelector('.ctn');
+// Прикрепляем его к «наблюдателю»
+observer.observe(el);
