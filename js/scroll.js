1a12873d30a69b25f74638852086100c9196744b
Vlad <ananevvlad12@gmail.com>
2022-12-14 22:38:54 +0200

    del console

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,8 @@
 
-
-
-
-// Создаем новый observer (наблюдатель)
 let observer = new IntersectionObserver(function (entries) {
     entries.forEach(function (entry) {
-// Выводим в консоль сам элемент
-        console.log(entry.target);
-// Выводим в консоль true (если элемент виден) или false (если нет)
-        console.log(entry.isIntersecting);
+
+     
         if (!entry.isIntersecting)
         {
             ctn.classList.add('hide');
@@ -20,8 +14,6 @@ let observer = new IntersectionObserver(function (entries) {
     });
 });
 
-// Задаем элемент для наблюдения
 let el = document.querySelector('header');
 const ctn = document.querySelector('.ctn');
-// Прикрепляем его к «наблюдателю»
 observer.observe(el);
