be5983b52a62d8dc851dc72996643ffed3dcbb8f
Vlad <ananevvlad12@gmail.com>
2022-12-26 23:36:49 +0200

    script

---
+++ js/arrow.js
@@ -0,0 +1,16 @@
+let observer = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+
+
+        if (!entry.isIntersecting) {
+            ctn.classList.add('hide');
+        }
+        if (entry.isIntersecting) {
+            ctn.classList.remove('hide');
+        }
+    });
+});
+
+let el = document.querySelector('header');
+const ctn = document.querySelector('.arrow');
+observer.observe(el);
